956b0ed36ef7aef87656fdcd6fe9320955db808a
bruski <292803281@qq.com>
2022-03-18 18:01:07 +0800

    feat(01): add practice of PracticeAssignmentOperator.cpp

--- 01_AssignmentOperator/PracticeAssignmentOperator.cpp
+++ 01_AssignmentOperator/PracticeAssignmentOperator.cpp
@@ -20,57 +20,60 @@ https://github.com/zhedahht/CodingInterviewChinese2/blob/master/LICENSE.txt)
 
 class CMyString {
 public:
-    CMyString(char *pData = nullptr);
-
+    CMyString(char *ch = nullptr);
     CMyString(const CMyString &str);
-
     ~CMyString(void);
 
     CMyString &operator=(const CMyString &str);
 
     void Print();
-
 private:
-    char *m_pData;
+    char* m_pData;
 };
 
 CMyString::CMyString(char *pData) {
     if (pData == nullptr) {
         m_pData = new char[1];
         m_pData[0] = '\0';
-    } else {
-        int length = strlen(pData);
-        m_pData = new char[length + 1];
-        strcpy(m_pData, pData);
+        return;
     }
-}
-
-CMyString::CMyString(const CMyString &str) {
-    int length = strlen(str.m_pData);
-    m_pData = new char[length + 1];
-    strcpy(m_pData, str.m_pData);
-}
-
-CMyString::~CMyString() {
-    delete[] m_pData;
+    int len = strlen(pData);
+    m_pData = new char[len + 1];
+    strcpy(m_pData, pData);
 }
 
 CMyString &CMyString::operator=(const CMyString &str) {
-    if (this == &str)
+    if (&str == this) {
         return *this;
-
-    delete[]m_pData;
-    m_pData = nullptr;
-
-    m_pData = new char[strlen(str.m_pData) + 1];
+    }
+    delete []m_pData;
+    int len = strlen(str.m_pData);
+    m_pData = new char[len + 1];
     strcpy(m_pData, str.m_pData);
 
     return *this;
 }
 
+
 // ====================测试代码====================
 void CMyString::Print() {
     printf("%s", m_pData);
+}
+
+CMyString::~CMyString() {
+    delete []m_pData;
+}
+
+void TestNullPtr() {
+    printf("TestNullPtr begins:\n");
+
+    CMyString str1(nullptr);
+
+    printf("The expected result is: %s.\n", "\0");
+
+    printf("The actual result is: ");
+    str1.Print();
+    printf(".\n");
 }
 
 void Test1() {
@@ -84,7 +87,12 @@ void Test1() {
 
     printf("The expected result is: %s.\n", text);
 
-    printf("The actual result is: ");
+    printf("The actual result of str1 is: ");
+    str1.Print();
+    printf(".\n");
+
+
+    printf("The actual result of str2 is: ");
     str2.Print();
     printf(".\n");
 }
@@ -105,7 +113,7 @@ void Test2() {
     printf(".\n");
 }
 
-// 连续赋值
+//// 连续赋值
 void Test3() {
     printf("Test3 begins:\n");
 
@@ -129,6 +137,7 @@ void Test3() {
 }
 
 int main(int argc, char *argv[]) {
+    TestNullPtr();
     Test1();
     Test2();
     Test3();
---
+++ 01_AssignmentOperator/cmake-build-debug/CMakeFiles/01_AssignmentOperator.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "01_AssignmentOperator"
+  "01_AssignmentOperator.pdb"
+  "CMakeFiles/01_AssignmentOperator.dir/PracticeAssignmentOperator.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/01_AssignmentOperator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
